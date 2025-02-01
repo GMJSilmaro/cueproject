638e1e79dccc45f6110b0e898c556d0c4615ea15
GMJSilmaro <dzheysilmaro@gmail.com>
2025-02-02 01:35:03 +0800

    Configure Next.js for deployment and add server actions and image domains

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,17 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'cueprojectdjs-git-main-gilberts-projects-584a51b2.vercel.app', 'cueprojectdjs-git-main-gilberts-projects-584a51b2.vercel.app', 'https://cueprojectdjs.vercel.app/']
+    }
+  },
   webpack: (config) => {
     config.resolve.alias['@radix-ui/react-select'] = path.resolve(__dirname, 'node_modules/@radix-ui/react-select');
     return config;
   },
   images: {
-    domains: ['picsum.photos', 'api.dicebear.com'],
+    domains: ['picsum.photos', 'api.dicebear.com', 'lh3.googleusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -21,6 +26,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
